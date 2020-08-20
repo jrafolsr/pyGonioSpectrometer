ccdc009c0c4702f184452ba6646d5043ccb1c61b
Joan <joanrafols1988@gmail.com>
2020-08-20 11:37:15 +0200

    Adds and mods for a more complete time series measurement.

--- gonio_measurement_time.py
+++ gonio_measurement_time.py
@@ -4,43 +4,166 @@ Created on Fri Jun 12 11:12:33 2020
 
 @author: OPEGLAB
 """
-from pyGonioSpectrometer import gonio_measurement
-from pyGonioSpectrometer.instrumentation import list_spectrometers
+#%%
+from pyGonioSpectrometer import gonio_measurement, write_to_file
+from pyGonioSpectrometer.instrumentation import list_spectrometers, SpectraMeasurement, ArduinoMotorController
 
 from time import sleep, time
+import numpy as np
+from datetime import datetime
+import os
+from os.path import join as pjoin
 
+SATURATION_COUNTS = 65535 # Saturation limit of the spectrometer
+UPPER_LIM = 58000 # Max. number of counts allowed before reducing the integration time
+LOWER_LIM = 40000 # Min. number of counts allowed before incresing the integration time
+MAX_TIME = 5000 # Max. time allowed per total adquisition (number of spectra x inegration time) in ms
+#%%
+
+
+# Define folder and filename
+folder = pjoin(r'C:\Users\OPEGLAB\Documents\data\goniospectrometer')    
+filename = 'default_time-series'
+# %%
+
+# Gonio measurement
+name_motor = 'ASRL7::INSTR'
+angle_step = 5
+angle_max = 5
+
+# Define variables for the spectrometer measurements
+# Assuming there is only one spectrometer, so taking the first element
+name_spectrometer = list_spectrometers()[0]
+integration_time = 100
+n_spectra = 1
+
+
+# Define variables for the time series
+interval_luminance = 10 # interval to take luminance measurements
+interval_gonio = 300 # # interval to take gonio measurements
+
+
+def gonio_time_series(filename, folder,\
+                      integration_time, n_spectra, interval_gonio,\
+                      angle_step = angle_step, angle_max = angle_max,\
+                      interval_luminance = interval_luminance,
+                      name_motor = name_motor, name_spectrometer = name_spectrometer
+                      ):
+
+    # Define general variables
+    disable_gonio = False # Gonio always active
+    plot = False  # No plotting
     
-def gonio_time_series(interval, name_motor,angle_max, angle_step,\
-                      name_spectrometer, integration_time, n_spectra,\
-                      filename = 'gonio_mesurement', folder = '.',\
-                      disable_gonio = False, plot = False):
+    # Raise error if the time to sample the spectra is shorter than the time to take it
+    if (n_spectra * integration_time/1000) >= (interval_luminance * 0.9):
+        raise Exception('ERROR: The interval_luminance is less than 90 % of the time need to take the spectra, consider reducing n_spectra, integration_time or increasing interval_luminance')
+
+    time_zero = time() # General inital timer
+    
+    # Initialize counters and flags
+    shutter_open = False
     k = 0
+    
     while True:
+        k += 1
+        
+        # Adaptative interval_luminance
+        total_ellapsed_time = time() - time_zero
+        if total_ellapsed_time > 1200: # After 20 min, take forward luminance every min
+            interval_luminance = min(60, interval_gonio)
+        elif total_ellapsed_time > 300:  # After 2 min, take forward luminance every 30 seconds min
+            interval_luminance = 30     
+#            
+        ninterval = interval_gonio // interval_luminance
+        
         try:
-            start_time = time()
+            print(f'\n\t<<<<< INFO: Measuring L0  every {interval_luminance:.0f} s >>>>>')
+            gonio = ArduinoMotorController(name_motor)
             
-            print(f'\n\n<<<<<INFO: Taking measurement #{k:d}>>>>>')
-                  
+            if not gonio.motor.bytes_in_buffer == 0:
+                print(gonio.motor.read())
+                
+            flame = SpectraMeasurement(name_spectrometer, integration_time = integration_time, n_spectra = n_spectra)
+            
+            # Timestamps for the header and filename
+            itimestamp = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+            timestamp = datetime.now().strftime("%Y-%m-%dT%Hh%Mm%Ss_")
+            start_luminance_adquisition = time()
+            path = pjoin(folder, timestamp + filename + '_L0.dat')
+        
+            with open(path, 'a') as f:
+                f.write(itimestamp + ' # Timestamp at the beginning of the measurement\n')
+                f.write(f'{integration_time:.0f} # Integration time in (ms)\n')
+                f.write(f'{n_spectra} # Number of spectra taken\n')
+                f.write(f'# Rel.time\t Integration time\t Wavelengths\n')
+                
+            # Take the dark spectra at zero
+            wavelengths = flame.get_wavelengths()
+            write_to_file(np.nan, integration_time, wavelengths, path)
+            intensities = flame.get_averaged_intensities()
+            write_to_file(np.nan, integration_time, intensities, path)
+            
+            print('INFO: Opening shutter')
+            gonio.move_shutter()
+            shutter_open = True
+            sleep(0.5)
+            
+            for i in range(ninterval):
+                start_time = time()
+                print(f'INFO: Taking spectra n.{i + 1: 2d} at forward luminance.')
+                
+                ellapsed_time_since_bkg = time() - start_luminance_adquisition
+                intensities = flame.get_averaged_intensities()
+                            
+                write_to_file(ellapsed_time_since_bkg, integration_time, intensities, path)
+                ellapsed_time = time() - start_time
+                
+                # Wait the amount of time specified by interval_luminance
+                while ellapsed_time < interval_luminance:
+                    ellapsed_time = time() - start_time
+                    sleep(0.001)
+
+            # Prepare for the gonio measurement.       
+            # Checking if the integration time needs to be increased or decreased
+            max_counts = flame.get_intensities().max()
+            
+            if max_counts <= LOWER_LIM:
+                print('INFO: Lower limit reach, incresing integration time')
+                integration_time = min(int(UPPER_LIM / max_counts * integration_time), MAX_TIME)
+                if integration_time * n_spectra > MAX_TIME:
+                    n_spectra = max(1, int(MAX_TIME / integration_time))
+                    
+            elif max_counts >= 0.9 * SATURATION_COUNTS:
+                print('INFO: Saturation reach, decreasing integration time')
+                while max_counts >= 0.9 * SATURATION_COUNTS:
+                    integration_time *= 0.95
+                    flame.config(integration_time)
+                    max_counts = flame.get_intensities().max()
+                    
+                integration_time = max(int(integration_time), 10)
+                n_spectra = min(20, int(MAX_TIME / integration_time))
+            
+            print(f'INFO: The adquisition is set to be  {n_spectra} x {integration_time} ms') 
+            # Close the sutter as the next iteration will be the gonio measurement
+            print('INFO: Closing shutter')
+            gonio.move_shutter()
+            shutter_open = False
+            sleep(2.0)
+            gonio.close()   
+            flame.close()
+            
+           
+            # The gonio measurement itself
+            shutter_open = True
+            print(f'\n\t<<<<< INFO: Taking measurement #{k//ninterval:d} >>>>>')
+            # Gonio measurements
             gonio_measurement(name_motor,angle_max, angle_step,\
                       name_spectrometer, integration_time, n_spectra,\
                       filename = filename, folder = folder,\
                       disable_gonio = disable_gonio, plot = plot)
+            shutter_open = False
+
             
-            ellapsed_time = time() - start_time
-            
-            
-            print(f'INFO: The measurement lasted {ellapsed_time:.1f} s')
-            
-            sleeping_time = interval - ellapsed_time
-            
-            print(f'      Going to sleep for {sleeping_time:.0f} s')
-            
-            
-            while ellapsed_time < interval:
-                sleep(0.1)
-                ellapsed_time = time() - start_time
-                
-            k += 1
             
         except KeyboardInterrupt:
             print('INFO: Measurement interrupted by the user')
@@ -49,24 +172,16 @@ def gonio_time_series(interval, name_motor,angle_max, angle_step,\
             print(e)
             break
         
+    if shutter_open:
+        gonio.move_shutter()
+        
+    flame.close()
+    gonio.close()
+    
 if __name__ == '__main__':
-    
-    # This will only be used if the script is called as it is, not if it is used as a function
-    name_motor = 'ASRL7::INSTR'
-    # Assuming there is only one spectrometer, so taking the first element
-    name_spectrometer = list_spectrometers()[0]
-    # Define measurement variables
-    folder = r'C:\Users\OPEGLAB\Documents\data\goniospectrometer\joan'
-    filename = 'LEC_E01D2_time-series'
-    angle_step = 10
-    angle_max = 80
-    # Define variables for the spectrometer measurements
-    integration_time = 200
-    n_spectra = 20
-    disable_gonio = False
-    plot = False
-    # Define variables for the time series
-    interval = 600 # interval to wait in seconds
-
-    gonio_time_series(interval, name_motor, angle_max, angle_step, name_spectrometer, integration_time, n_spectra,filename, folder, disable_gonio, plot)
-    
+    gonio_time_series(filename, folder,\
+                      integration_time, n_spectra, interval_gonio,\
+                      interval_luminance = interval_luminance,\
+                      angle_step = angle_step, angle_max = angle_max,\
+                      name_motor = name_motor, name_spectrometer = name_spectrometer
+                      )
