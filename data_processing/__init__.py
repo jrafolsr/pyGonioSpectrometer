b5213d9cc663048a928bdc7b39e697ea07a8994d
Joan <joanrafols1988@gmail.com>
2021-02-03 09:14:46 +0100

    Added the angular emission polar plot function

--- data_processing/__init__.py
+++ data_processing/__init__.py
@@ -5,5 +5,5 @@ Created on Fri Nov 29 16:38:13 2019
 @author: JOANRR
 """
 
-from .data_processing import process_goniodata, process_L0, plot_spectral_evolution, load_sri_file, plot_sri_map
+from .data_processing import process_goniodata, process_L0, plot_spectral_evolution, load_sri_file, plot_sri_map, plot_angular_emission
 from .data_fitting import error_landscape, fit_thickness, min_error_profile, interpolate_expdata, load_simdata, gci, compare_data, fit_forward_position
